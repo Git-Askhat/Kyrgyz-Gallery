68645ef422b63f77fb61eab1f10d32adae69e426
Git-Askhat <askat.shailoobekovich@mail.ru>
2022-01-31 03:41:54 +0600

    added carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components'
 
 import Photos from './photos/index';
+import PhotosComponent from '../components/photos/index';
 import './index.scss';
 
 export default function App() {
   return (
     <div>
       <Global />
-      <Photos />
+      <PhotosComponent />
       <h1>Hello World</h1>
     </div>
   );
